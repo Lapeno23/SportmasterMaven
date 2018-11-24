e9c82064065571e42c1cb0000be463a00bd5dc2a
Lapeno23 <RVYakovenko@stud.kai.ru>
2018-11-24 12:15:22 +0300

    SportMaster

---
+++ src/main/java/ru/itpark/BonusService.java
@@ -0,0 +1,29 @@
+package ru.itpark;
+
+public class BonusService {
+    public int calculateBonus(int purchaseAmount, int historyTotalAmount) {
+        int result = 0;
+        int blueLevelBonus = 50;
+        int silverLevelBonus = 70;
+        int goldLevelBonus = 100;
+        int minPurchaseAmountForBonus = 1000;
+        int maxHistoryTotalAmountForBlue = 15_000;
+        int minPurchaseAmountForSilverBonus = 15_001;
+        int maxHistoryTotalAmountForSilver = 150_000;
+        int minPurchaseAmountForGold = 150_001;
+        int purchaseSum = purchaseAmount + historyTotalAmount;
+        int bonusIndex = purchaseAmount / minPurchaseAmountForBonus;
+
+        if (purchaseSum >= minPurchaseAmountForBonus && purchaseSum <= maxHistoryTotalAmountForBlue) {
+            System.out.println("Blue card");
+            return bonusIndex * blueLevelBonus;
+        } else if (purchaseSum >= minPurchaseAmountForSilverBonus && purchaseSum <= maxHistoryTotalAmountForSilver) {
+            System.out.println("Silver card");
+            return bonusIndex * silverLevelBonus;
+        } else if (purchaseSum >= minPurchaseAmountForGold) {
+            System.out.println("Gold card");
+            return bonusIndex * goldLevelBonus;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/itpark/BonusServiceTest.java
@@ -0,0 +1,19 @@
+package ru.itpark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void calculateBonus() {
+        BonusService bonusService = new BonusService();
+        assertEquals("Silver card", "Silver card");
+        assertEquals(10_430, bonusService.calculateBonus(149_000,1));
+        assertEquals("Blue card", "Blue card");
+        assertEquals(500, bonusService.calculateBonus(10_000, 4_000));
+        assertEquals("Gold card", "Gold card");
+        assertEquals(15_000, bonusService.calculateBonus(150_000, 40_000));
+    }
+}
